0f5bf5ac35557246410d2db13d5e9d2e5980a1dc
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-03-15 13:31:49 -0400

    Adds sample output file and rake task.

---
+++ test/proof.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + "/../lib/crayon"
+
+Crayon.puts.bold_underline_blue_on_yellow("This should be formatted the same as the line below")
+Crayon.puts.on_yellow_bold_blue_underline("This should be formatted the same as the line above")
