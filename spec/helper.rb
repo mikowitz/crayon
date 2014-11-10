e8775d95786f9f751a5460b6a5d4e3395725f20f
Michael Berkowitz <michael.berkowitz@gmail.com>
2014-11-10 15:33:06 -0500

    Remove unused spec helper files

--- spec/helper.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift File.dirname(__FILE__)
-$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
-require 'crayon'
-require 'spec'
-require 'spec/autorun'
-
-Spec::Runner.configure do |config|
-end
